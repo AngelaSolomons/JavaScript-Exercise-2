9bc9b2f23f027e249a34694fd89c28680041fdd8
Angela Solomons <angelasolomons03@gmail.com>
2026-06-01 16:08:04 +0200

    Updated files.

---
+++ script.js
@@ -0,0 +1,31 @@
+// function sum(()) {
+
+// }
+
+
+// function factorial(num) {
+//     if num 
+// }
+
+
+// function funkyMath(a, b) {
+
+// }
+
+let nums = [1, 2, 33, 45, 6, 44]
+
+for (let i = 0; i < nums.length; i++) {
+    if i % 3 === 0 {
+        let sorted = nums.toSorted();
+        console.log(sorted);
+           
+    } 
+}
+
+let me = {
+    firstName: "Angela",
+    lastName: "Solomons",
+    age: "1",
+    favColour: "yellow",
+    dreamCar: "Porsche",
+}
