cff638e4acc739afb0645e39de4aec555221881f
Angela Solomons <angelasolomons03@gmail.com>
2026-06-02 15:15:18 +0200

    Update

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-// function sum(()) {
+// Sum function - first check if parameter is an integer
+// function sum() {
 
 // }
 
+// Factorial function - takes a number and returns the factorial of that number
+function factorial(num) {
+    let result = 1;
+    for (let i = num; i > 0; i--) {
+        result *= i;
+    }
+    return result;
+}
+console.log(`Factorial of 4: ${factorial(4)}`);
 
-// function factorial(num) {
-//     if num 
+// // funkyMath Function - 
+// function funkyMath(a, b, c, d) {
+//     if 
 // }
 
-
-// function funkyMath(a, b) {
-
-// }
-
+// Loop that removes odd numbers from string and adds them to a new array from smallest to biggest.
 let nums = [1, 2, 33, 45, 6, 44]
+let oddNums = []
 
-for (let i = 0; i < nums.length; i++) {
-    if i % 3 === 0 {
-        let sorted = nums.toSorted();
-        console.log(sorted);
-           
+for (let i = nums.length -1; i >= 0; i--) {
+    if (nums[i] % 2 !== 0) {
+        oddNums.push(nums.splice(i, 1)[0]);
     } 
 }
 
+oddNums.sort((a, b) => a - b);
+console.log(`Even numbers: ${nums}`);
+console.log(`Odd numbers: ${oddNums}`);
+
+
+// Create object with properties of first name, last name, age, favourite colour, dream car.
 let me = {
     firstName: "Angela",
     lastName: "Solomons",
-    age: "1",
+    age: "50",
     favColour: "yellow",
     dreamCar: "Porsche",
 }
+console.log(me);
+
+// Add a new property - favourite food
+me.favouriteFood = "Sushi";
+console.log(me);
+
+// Delete the age property
+delete me.age;
+console.log(me);
