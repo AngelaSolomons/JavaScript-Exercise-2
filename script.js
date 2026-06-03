2c4370e25a2237c42b6ad8457526ea94ccf66d00
AngelaSolomons <angelasolomons03@gmail.com>
2026-06-03 22:13:34 +0200

    Updated functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Sum function - first check if parameter is an integer
-// function sum() {
+function sum(num) {
 
-// }
+    if (typeof num !== "number" || num % 1 !== 0) {
+        return "The value passed is not a number";
+    }
+
+    let total = 0;
+
+    for (let i = 0; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
+
+console.log(`Sum: ${sum(5)}`);
+
 
 // Factorial function - takes a number and returns the factorial of that number
 function factorial(num) {
@@ -11,12 +24,32 @@ function factorial(num) {
     }
     return result;
 }
+
 console.log(`Factorial of 4: ${factorial(4)}`);
 
+
 // // funkyMath Function - 
-// function funkyMath(a, b, c, d) {
-//     if 
-// }
+function funkyMath(a, b, c, d) {
+
+    if (arguments.length === 2) {
+        return b - a;
+    }
+    if (arguments.length === 3) {
+        return a + b + c;
+    }
+    if (arguments.length === 4) {
+        let pair1 = a + b;
+        let pair2 = c + d;
+        return pair1 / pair2;
+    }
+    return "Invalid. Please provide 2, 3, or 4 numbers.";
+}
+
+console.log(`funkyMath(6, 12): ${funkyMath(6, 12)}`);
+console.log(`funkyMath(11, 2, 30): ${funkyMath(11, 2, 30)}`);
+console.log(`funkyMath(8, 21, 39, 5): ${funkyMath(8, 21, 39, 5)}`);
+console.log(`funkyMath(55): ${funkyMath(55)}`);
+
 
 // Loop that removes odd numbers from string and adds them to a new array from smallest to biggest.
 let nums = [1, 2, 33, 45, 6, 44]
@@ -37,8 +70,8 @@ console.log(`Odd numbers: ${oddNums}`);
 let me = {
     firstName: "Angela",
     lastName: "Solomons",
-    age: "50",
-    favColour: "yellow",
+    age: 50,
+    favColour: "Yellow",
     dreamCar: "Porsche",
 }
 console.log(me);
